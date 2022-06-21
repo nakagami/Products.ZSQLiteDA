9dd9ca26b762b375d58b2210bb9aeda2368735f8
Georg Pfolz <georg.pfolz@invesy.at>
2022-06-21 13:49:54 +0200

    DA can use an external DB (outside instance folder)

--- Products/ZSQLiteDA/DA.py
+++ Products/ZSQLiteDA/DA.py
@@ -10,114 +10,176 @@
 # FOR A PARTICULAR PURPOSE
 #
 ##############################################################################
-database_type='SQLite'
+database_type = 'SQLite'
 
-import sys
+import os
 from _thread import allocate_lock
-
-from .db import DB, manage_DataSources, data_dir
+from html import escape
+from AccessControl.Permissions import change_database_connections
+from AccessControl.SecurityInfo import ClassSecurityInfo
+# from OFS.PropertyManager import PropertyManager
+from App.Dialogs import MessageDialog
+from .db import DB, manage_DataSources, init_new_db, DEFAULT_DATA_DIR
 import Shared.DC.ZRDB.Connection
 from App.special_dtml import HTMLFile
 import Acquisition
 from ExtensionClass import Base
 
-_connections={}
-_connections_lock=allocate_lock()
+_connections = {}
+_connections_lock = allocate_lock()
 
-data_sources=manage_DataSources
+data_sources = manage_DataSources
 
-addConnectionForm=HTMLFile('dtml/connectionAdd',globals())
+addConnectionForm = HTMLFile('dtml/connectionAdd', globals())
 
 
 def manage_addZSQLiteConnectionForm(self, REQUEST, *args, **kw):
     """Add a connection form"""
+
     return addConnectionForm(
         self, self, REQUEST,
         database_type=database_type,
-        data_dir=data_dir,
-        data_sources=data_sources)
+        data_dir=DEFAULT_DATA_DIR,
+        data_sources=data_sources(data_dir=DEFAULT_DATA_DIR))
 
 
-def manage_addZSQLiteConnection(
-    self, id, title, connection, REQUEST=None):
+def manage_addZSQLiteConnection(self, id, title, connection, REQUEST=None):
     """Add a DB connection to a folder"""
 
     # Note - ZSQLiteDA's connect immediately check is alway false
     self._setObject(id, Connection(
-        id, title, connection, None))
-    if REQUEST is not None: return self.manage_main(self,REQUEST)
+        id, title, connection, REQUEST))
+    if REQUEST is not None:
+        return self.manage_main(self, REQUEST)
 
 
 class Connection(Shared.DC.ZRDB.Connection.Connection):
     " "
-    
+
+    security = ClassSecurityInfo()
     _isAnSQLConnection = True
-    
-    manage_options = Shared.DC.ZRDB.Connection.Connection.manage_options+(
+    data_dir = DEFAULT_DATA_DIR
+
+    def __init__(self, id, title, connection_string, REQUEST=None):
+        data_dir = REQUEST.get('data_dir', DEFAULT_DATA_DIR)
+
+        init_new_db(data_dir=data_dir, connection_string=connection_string)
+
+        self.data_dir = data_dir
+
+        super().__init__(id, title, connection_string)
+
+    def data_dir_is_default(self):
+        return self.data_dir == DEFAULT_DATA_DIR
+
+    manage_options = Shared.DC.ZRDB.Connection.Connection.manage_options + (
         {'label': 'Browse', 'action': 'manage_browse'},
         # {'label': 'Design', 'action':'manage_tables'},
-        )
+    )
 
-    manage_browse=HTMLFile('dtml/browse',globals())
+    manage_browse = HTMLFile('dtml/browse', globals())
 
-    database_type=database_type
-    id='%s_database_connection' % database_type
-    meta_type=title='Z %s Database Connection' % database_type
+    database_type = database_type
+    id = f'{database_type}_database_connection'
+    meta_type = title = f'Z {database_type} Database Connection'
     zmi_icon = 'fas fa-database'
-    data_dir=data_dir
 
-    manage_properties=HTMLFile('dtml/connectionEdit', globals(),
-                                       data_sources=data_sources)
+    manage_main = HTMLFile(
+        'dtml/connectionStatus', globals()
+    )
+
+    manage_properties = HTMLFile(
+        'dtml/connectionEdit', globals(),
+        data_sources=data_sources,
+        DEFAULT_DATA_DIR=DEFAULT_DATA_DIR
+    )
+
+    @security.protected(change_database_connections)
+    def manage_edit(self, title, data_dir, connection_string='',
+                    new_database='', check=None, REQUEST=None):
+        """Change connection
+        """
+        if new_database:
+            connection_string = new_database
+
+        if data_dir != self.data_dir:  # data_dir is changed
+            connection_string = '' or new_database
+
+        if data_dir == DEFAULT_DATA_DIR:
+            init_new_db(data_dir=data_dir, connection_string=connection_string)
+        elif not os.path.exists(data_dir):
+            return MessageDialog(
+                title='Edited',
+                message=f'The directory <code>{self.data_dir}</code> does not exist. Please create it on the file system.',
+                action='./manage_properties')
+
+        if not os.path.exists(os.path.join(data_dir, connection_string)):
+            return MessageDialog(
+                title='Edited',
+                message=f'The database file <strong>{connection_string}</strong> does not exist in {data_dir}. Please create it on the file system.',
+                action='./manage_properties')
+
+        self.data_dir = data_dir
+
+        if self.connected():
+            self.manage_close_connection()
+
+        self.edit(title, connection_string, check)
+
+        if REQUEST is not None:
+            esc_id = escape(self.id)
+            return MessageDialog(
+                title='Edited',
+                message=f'<strong>{esc_id}</strong> has been edited.',
+                action='./manage_properties')
 
     def connected(self):
         if hasattr(self, '_v_database_connection'):
             return self._v_database_connection.opened
         return ''
 
-    def connect(self,s):
+    def connect(self, s):
         _connections_lock.acquire()
         try:
-            c=_connections
-            self._v_database_connection=c[s]=DB(s)
+            c = _connections
+            self._v_database_connection = c[s] = DB(s)
             return self
         finally:
             _connections_lock.release()
 
     def tpValues(self):
-        r=[]
+        r = []
         if not hasattr(self, '_v_database_connection'):
             return r
-        c=self._v_database_connection
+        c = self._v_database_connection
         try:
             for d in c.tables(rdb=0):
                 try:
-                    name=d['TABLE_NAME']
-                    b=TableBrowser()
-                    b.__name__=name
-                    b._d=d
-                    b._c=c
+                    name = d['TABLE_NAME']
+                    b = TableBrowser()
+                    b.__name__ = name
+                    b._d = d
+                    b._c = c
                     # b._columns=c.columns(name)
-                    try: b.icon=d['TABLE_TYPE']
-                    except: pass
+                    try:
+                        b.icon = d['TABLE_TYPE']
+                    except Exception:
+                        pass
                     r.append(b)
                     # tables[name]=b
-                except:
+                except Exception:
                     # print d['TABLE_NAME'], sys.exc_type, sys.exc_value
                     pass
 
-        finally: pass #print sys.exc_type, sys.exc_value
-        #self._v_tpValues=r
+        finally:
+            pass  # print sys.exc_type, sys.exc_value
+        # self._v_tpValues=r
         return r
 
     def __getitem__(self, name):
-        if name=='tableNamed':
-            if not hasattr(self, '_v_tables'): self.tpValues()
-            return self._v_tables.__of__(self)
-        raise KeyError(name)
-
-    def __getitem__(self, name):
-        if name=='tableNamed':
-            if not hasattr(self, '_v_tables'): self.tpValues()
+        if name == 'tableNamed':
+            if not hasattr(self, '_v_tables'):
+                self.tpValues()
             return self._v_tables.__of__(self)
         raise KeyError(name)
 
@@ -133,38 +195,59 @@ class Connection(Shared.DC.ZRDB.Connection.Connection):
     def manage_update(self, table, keys, cols, REQUEST=None):
         """Create an SQL update"""
 
+
 class TableBrowserCollection(Acquisition.Implicit):
     "Helper class for accessing tables via URLs"
 
+
 class Browser(Base):
     def __getattr__(self, name):
-        try: return self._d[name]
-        except KeyError: raise AttributeError(name)
+        try:
+            return self._d[name]
+        except KeyError:
+            raise AttributeError(name)
+
 
 class TableBrowser(Browser, Acquisition.Implicit):
-    icon='what'
-    Description=check=''
+    icon = 'what'
+    Description = check = ''
 
     def tpValues(self):
-        r=[]
-        tname=self.__name__
-        b=SQLBrowser()
+        r = []
+        tname = self.__name__
+        b = SQLBrowser()
         b._parent = self
         b._d = {'SQL': self._c.sqlscript(tname)}
         r.append(b)
         return r
 
-    def tpId(self): return self._d['TABLE_NAME']
-    def tpURL(self): return "Table/%s" % self._d['TABLE_NAME']
-    def Name(self): return self._d['TABLE_NAME']
-    def Type(self): return self._d['TABLE_TYPE']
+    def tpId(self):
+        return self._d['TABLE_NAME']
+
+    def tpURL(self):
+        return "Table/%s" % self._d['TABLE_NAME']
+
+    def Name(self):
+        return self._d['TABLE_NAME']
+
+    def Type(self):
+        return self._d['TABLE_TYPE']
 
 
 class SQLBrowser(Browser):
-    icon=None
+    icon = None
 
-    def tpId(self): return "SQL"
-    def tpURL(self): return "Table/%s/SQL" % self._parent._d['TABLE_NAME']
-    def Name(self): return ''
-    def Type(self): return ''
-    def Description(self): return self._d['SQL']
+    def tpId(self):
+        return "SQL"
+
+    def tpURL(self):
+        return "Table/%s/SQL" % self._parent._d['TABLE_NAME']
+
+    def Name(self):
+        return ''
+
+    def Type(self):
+        return ''
+
+    def Description(self):
+        return self._d['SQL']
--- Products/ZSQLiteDA/db.py
+++ Products/ZSQLiteDA/db.py
@@ -18,112 +18,165 @@ import Shared.DC.ZRDB.THUNK
 import App.ApplicationManager
 
 instancepath = App.ApplicationManager.getConfiguration().instancehome
-data_dir = os.path.abspath(os.path.join(instancepath, 'var', 'sqlite'))
+DEFAULT_DATA_DIR = os.path.abspath(os.path.join(instancepath, 'var', 'sqlite'))
 
-def manage_DataSources():
+
+def manage_DataSources(data_dir=DEFAULT_DATA_DIR):
+    if os.sep not in data_dir:
+        data_dir = os.path.join(DEFAULT_DATA_DIR, data_dir)  # create a subdirectory DEFAULT_DATA_DIR
 
     if not os.path.exists(data_dir):
-        try:
-            os.mkdir(data_dir)
-        except:
+        if data_dir != DEFAULT_DATA_DIR:
             raise sqlite3.OperationalError(
-                """
+                f"""
                 The Zope SQLite Database Adapter requires the
-                existence of the directory, <code>%s</code>.  An error
-                occurred  while trying to create this directory.
-                """ % data_dir)
+                existence of the directory, <code>{data_dir}</code>.
+                Please create it on the file system.
+                """)
+        elif not os.path.exists(DEFAULT_DATA_DIR):
+            # it's quite safe to create the default path
+            try:
+                os.mkdir(DEFAULT_DATA_DIR)
+            except Exception:
+                raise sqlite3.OperationalError(
+                    f"""
+                    The Zope SQLite Database Adapter requires the
+                    existence of the directory, <code>{data_dir}</code>.  An error
+                    occurred  while trying to create this directory.
+                    """)
+
     if not os.path.isdir(data_dir):
         raise sqlite3.OperationalError(
-            """
+            f"""
             The Zope SQLite Database Adapter requires the
-            existence of the directory, <code>%s</code>.  This
+            existence of the directory, <code>{data_dir}</code>.  This
             exists, but is not a directory.
-            """ % data_dir)
+            """)
 
     return map(
-        lambda d: (d,''),
+        lambda d: (d, ''),
         filter(lambda f, i=os.path.isfile, d=data_dir, j=os.path.join:
-               i(j(d,f)),
+               i(j(d, f)),
                os.listdir(data_dir))
-        )
+    )
+
+
+def init_new_db(data_dir, connection_string):
+    if data_dir == DEFAULT_DATA_DIR:
+        if not os.path.exists(DEFAULT_DATA_DIR):
+            os.mkdir(DEFAULT_DATA_DIR)
+    else:
+        raise sqlite3.OperationalError(
+            f"""The Zope SQLite Database Adapter requires the
+existence of the file <code>{connection_string}</code> in
+the directory <code>{data_dir}</code>.
+
+For security reasons, you are allowed to create new
+databases in the default sqlite directory only:
+{DEFAULT_DATA_DIR}
+
+If you want to use a database at a different location,
+you have to manage the directory and the database files
+on the file system.""")
+
+    if connection_string:
+        db_path = os.path.join(data_dir, connection_string)
+
+        if os.path.exists(db_path):
+            pass
+
+        else:
+            with open(db_path, 'w') as f:
+                f.write('')
+
 
 class DB(Shared.DC.ZRDB.THUNK.THUNKED_TM):
 
-    opened=''
+    opened = ''
 
     def open(self):
         connection = self.connection
         if connection != ':memory:':
-            connection = os.path.join(data_dir, connection)
-        self.db = sqlite3.connect(connection, check_same_thread = False)
-        self.opened=DateTime()
+            connection = os.path.join(
+                DEFAULT_DATA_DIR,
+                connection
+            )
+        self.db = sqlite3.connect(connection, check_same_thread=False)
+        self.opened = DateTime()
 
     def close(self):
         self.db.close()
         self.opened = None
 
-    def __init__(self,connection):
-        self.connection=connection
+    def __init__(self, connection):
+        self.connection = connection
         self.open()
 
     def query(self, query_string, max_rows=None):
         self._begin()
         c = self.db.cursor()
 
-        queries=filter(None, [q.strip() for q in query_string.split('\0')])
+        queries = filter(None, [q.strip() for q in query_string.split('\0')])
         if not queries:
             raise sqlite3.OperationalError('empty query')
-        desc=None
-        result=[]
+        desc = None
+        result = []
         for qs in queries:
             c.execute(qs)
-            d=c.description
-            if d is None: continue
-            if desc is None: desc=d
+            d = c.description
+            if d is None:
+                continue
+            if desc is None:
+                desc = d
             elif d != desc:
                 raise sqlite3.OperationalError(
                     'Multiple incompatible selects in '
                     'multiple sql-statement query'
-                    )
+                )
 
             if max_rows:
-                if not result: result=c.fetchmany(max_rows)
+                if not result:
+                    result = c.fetchmany(max_rows)
                 elif len(result) < max_rows:
-                    result=result+c.fetchmany(max_rows-len(result))
+                    result = result + c.fetchmany(max_rows - len(result))
             else:
                 result = c.fetchall()
 
         self._finish()
-        if desc is None: return (),()
+        if desc is None:
+            return (), ()
 
-        items=[]
+        items = []
         for name, type, width, ds, p, scale, null_ok in desc:
-            if type=='NUMBER':
-                if scale==0: type='i'
-                else: type='n'
-            elif type=='DATE':
-                type='d'
-            else: type='s'
+            if type == 'NUMBER':
+                if scale == 0:
+                    type = 'i'
+                else:
+                    type = 'n'
+            elif type == 'DATE':
+                type = 'd'
+            else:
+                type = 's'
             items.append({
                 'name': name,
                 'type': type,
                 'width': width,
                 'null': null_ok,
-                })
+            })
 
         return items, result
 
     def _begin(self):
-        #sqlite3.connection don't have begein() method.
+        # sqlite3.connection don't have begein() method.
         pass
 
     def _finish(self):
         self.db.commit()
 
     def _abort(self):
-        conn.db.rollback()
+        self.db.rollback()
 
-    def tables(self,*args,**kw):
+    def tables(self, *args, **kw):
         self._begin()
         c = self.db.cursor()
         c.execute("SELECT name, type FROM sqlite_master WHERE type='table' or type='view'")
@@ -131,14 +184,17 @@ class DB(Shared.DC.ZRDB.THUNK.THUNKED_TM):
         result = []
         rs = c.fetchall()
         for r in rs:
-            result.append({'TABLE_NAME':r[0], 'TABLE_TYPE':r[1]})
+            result.append({
+                'TABLE_NAME': r[0],
+                'TABLE_TYPE': r[1]
+            })
         self._finish()
         return result
 
     def sqlscript(self, table_name):
         self._begin()
         c = self.db.cursor()
-        c.execute("SELECT sql FROM sqlite_master WHERE name='%s'" % table_name)
+        c.execute(f"SELECT sql FROM sqlite_master WHERE name='{table_name}'")
         sql = c.fetchone()[0]
         self._finish()
         return sql
